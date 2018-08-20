7f7b10ba2659d8224b379df8918215cf3d18eb00
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-08-20 14:12:01 +0200

    Create entrypoint.sh

---
+++ alpine-python/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo ""
+echo "$(cat /etc/banner)"
+
+echo ""
+echo "Alpine Build: ${ALPINE_VERSION}"
+echo "Container Hostname: $(hostname)"
+echo "Checkout my Docker Blogs: "
+echo "- https://sysadmins.co.za/tag/docker"
+echo "- http://blog.ruanbekker.com/blog/categories/docker"
+echo ""
+
+#exec "$@"
+exec "/bin/sh"
